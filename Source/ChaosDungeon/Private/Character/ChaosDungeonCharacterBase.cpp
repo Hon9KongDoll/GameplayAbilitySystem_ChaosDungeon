39ec2a6f9e3de211ec691fe5c6d3cf4e41fe6bc3
Huizhang <1483033869@qq.com>
2025-03-01 01:20:24 +0800

    The ChaosDungeonCharacterBase Class

---
+++ Source/ChaosDungeon/Private/Character/ChaosDungeonCharacterBase.cpp
@@ -0,0 +1,1 @@
+#include "Character/ChaosDungeonCharacterBase.h"
---
+++ Source/ChaosDungeon/Public/Character/ChaosDungeonCharacterBase.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ChaosDungeonCharacterBase.generated.h"
+
+UCLASS()
+class CHAOSDUNGEON_API AChaosDungeonCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+};
