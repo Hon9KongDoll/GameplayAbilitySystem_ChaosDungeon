d5a5ee3997f4b6bfb2e089e31d732330715cc985
Huizhang <1483033869@qq.com>
2025-03-01 10:53:51 +0800

    Character And Enemy Blueprint-Assets

--- Source/ChaosDungeon/Private/Character/ChaosDungeonCharacterBase.cpp
+++ Source/ChaosDungeon/Private/Character/ChaosDungeonCharacterBase.cpp
@@ -1,1 +1,14 @@
 #include "Character/ChaosDungeonCharacterBase.h"
+
+// Engine
+#include "Components/StaticMeshComponent.h"
+
+AChaosDungeonCharacterBase::AChaosDungeonCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	Weapon = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Weapon"));
+	Weapon->SetupAttachment(GetMesh(), FName(TEXT("Weapon_R")));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
--- Source/ChaosDungeon/Public/Character/ChaosDungeonCharacterBase.h
+++ Source/ChaosDungeon/Public/Character/ChaosDungeonCharacterBase.h
@@ -4,10 +4,18 @@
 #include "GameFramework/Character.h"
 #include "ChaosDungeonCharacterBase.generated.h"
 
+class UStaticMeshComponent;
+
 // Abstract宏 : 无法进行实例化，可能包含需要在子类中实现的纯虚函数
 UCLASS(Abstract)
 class CHAOSDUNGEON_API AChaosDungeonCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
+
+public:
+	AChaosDungeonCharacterBase();
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<UStaticMeshComponent> Weapon;
 };
