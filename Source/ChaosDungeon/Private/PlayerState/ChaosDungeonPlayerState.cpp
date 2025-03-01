b9c0d0d9fd86475b0c6898ec232f04104aa14055
Huizhang <1483033869@qq.com>
2025-03-01 16:03:44 +0800

    Character PlayerState

---
+++ Source/ChaosDungeon/Private/PlayerState/ChaosDungeonPlayerState.cpp
@@ -0,0 +1,7 @@
+#include "PlayerState/ChaosDungeonPlayerState.h"
+
+AChaosDungeonPlayerState::AChaosDungeonPlayerState()
+{
+	// 网络更新频率
+	SetNetUpdateFrequency(100.F);
+}
---
+++ Source/ChaosDungeon/Public/PlayerState/ChaosDungeonPlayerState.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ChaosDungeonPlayerState.generated.h"
+
+UCLASS()
+class CHAOSDUNGEON_API AChaosDungeonPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AChaosDungeonPlayerState();
+};
