9ef6a0d9e7140a00f149414e626e7dbcc828ecab
Huizhang <1483033869@qq.com>
2025-03-03 00:23:54 +0800

    Update AttributeSet Property

--- Source/ChaosDungeon/Private/AbilitySystem/ChaosDungeonAttributeSet.cpp
+++ Source/ChaosDungeon/Private/AbilitySystem/ChaosDungeonAttributeSet.cpp
@@ -1,2 +1,41 @@
 #include "AbilitySystem/ChaosDungeonAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UChaosDungeonAttributeSet::UChaosDungeonAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(100.f);
+	InitMaxMana(100.f);
+}
+
+void UChaosDungeonAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UChaosDungeonAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UChaosDungeonAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UChaosDungeonAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UChaosDungeonAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UChaosDungeonAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UChaosDungeonAttributeSet, Health, OldHealth);
+}
+
+void UChaosDungeonAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UChaosDungeonAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UChaosDungeonAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UChaosDungeonAttributeSet, Mana, OldMana);
+}
+
+void UChaosDungeonAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UChaosDungeonAttributeSet, MaxMana, OldMaxMana);
+}
