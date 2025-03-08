b898b7f43bec640694f57eefe74788a8df71f388
Huizhang <1483033869@qq.com>
2025-03-08 15:46:45 +0800

    InfinitesGameplayEffect

--- Source/ChaosDungeon/Private/Actor/ChaosDungeonEffectActor.cpp
+++ Source/ChaosDungeon/Private/Actor/ChaosDungeonEffectActor.cpp
@@ -17,27 +17,97 @@ void AChaosDungeonEffectActor::BeginPlay()
 	Super::BeginPlay();
 }
 
+void AChaosDungeonEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+
+	if (InfinitesEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+}
+
+void AChaosDungeonEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+
+	if (InfinitesEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+
+	if (InfinitesEffectRemovePolicy == EEffectRemovePolicy::RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent* AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		
+		if (AbilitySystemComponent)
+		{
+			TArray<FActiveGameplayEffectHandle> RemoveActiveGameplayEffectHandle;
+
+			for (TPair< FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveGameplayEffectHandle : ActiveGameplayEffectHandles)
+			{
+				if (ActiveGameplayEffectHandle.Value == AbilitySystemComponent)
+				{
+					AbilitySystemComponent->RemoveActiveGameplayEffect(ActiveGameplayEffectHandle.Key);
+					RemoveActiveGameplayEffectHandle.Add(ActiveGameplayEffectHandle.Key);
+				}
+			}
+
+			for (FActiveGameplayEffectHandle ActiveGameplayEffectHandle : RemoveActiveGameplayEffectHandle)
+			{
+				ActiveGameplayEffectHandles.FindAndRemoveChecked(ActiveGameplayEffectHandle);
+			}
+		}
+	}
+}
+
 void AChaosDungeonEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
 	check(GameplayEffectClass);
-	
+
 	UAbilitySystemComponent* AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if (AbilitySystemComponent == nullptr) return;
 
 	// 作用是生成一个效果上下文，用来存储施加者(Instigator)、来源对象(SourceObject)、命中目标等附加信息，以便 GameplayEffect 处理时使用
 	FGameplayEffectContextHandle GameplayEffectContextHandle = AbilitySystemComponent->MakeEffectContext();
 	GameplayEffectContextHandle.AddSourceObject(this);
-	
+
 	// 实例化一个 UGameplayEffect，并为其提供必要的上下文信息(如等级、来源信息)，以便后续应用到自己或目标
 	// FGameplayEffectSpecHandle MakeOutgoingSpec(TSubclassOf<UGameplayEffect> GameplayEffectClass, float Level, FGameplayEffectContextHandle Context);
 	// GameplayEffectClass : 用于指定要应用的 GameplayEffect,通常是在编辑器中创建的 GameplayEffect 资产
 	// Level : 指定 GameplayEffect 的等级，可以用于影响数值计算
 	// Context : 传递额外的上下文信息(如伤害来源、命中目标等)
 	FGameplayEffectSpecHandle GameplayEffectSpecHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, 1.f, GameplayEffectContextHandle);
-	
+
 	// 允许 Actor(通常是角色) 对自己应用一个 GameplayEffect(游戏效果)，用于修改属性、应用状态效果、触发伤害
 	// FActiveGameplayEffectHandle ApplyGameplayEffectSpecToSelf (const FGameplayEffectSpec& GameplayEffectSpec, FPredictionKey PredictionKey = FPredictionKey());
 	// FGameplayEffectSpec : 是 UGameplayEffect 的实例化版本，包含持续时间、强度、目标属性等数据
 	// PredictionKey : 主要用于网络预测同步, 对于客户端预测的 Gameplay Effect，可以用于防止延迟同步问题
-	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*GameplayEffectSpecHandle.Data.Get());
+	FActiveGameplayEffectHandle ActiveGameplayEffectHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*GameplayEffectSpecHandle.Data.Get());
+
+	// 判断游戏效果 GameplayEffectSpecHandle 是否是无限持续
+	if (GameplayEffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite)
+	{
+		// 判断无限效果移除策略是否为移除结束重叠
+		if (InfinitesEffectRemovePolicy == EEffectRemovePolicy::RemoveOnEndOverlap)
+		{
+			ActiveGameplayEffectHandles.Add(ActiveGameplayEffectHandle, AbilitySystemComponent);
+		}
+	}
 }
--- Source/ChaosDungeon/Public/Actor/ChaosDungeonEffectActor.h
+++ Source/ChaosDungeon/Public/Actor/ChaosDungeonEffectActor.h
@@ -2,9 +2,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ActiveGameplayEffectHandle.h"
 #include "ChaosDungeonEffectActor.generated.h"
 
 class UGameplayEffect;
+class UAbilitySystemComponent;
+
+// 效果应用策略
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+// 效果移除策略 - 仅限永久游戏效果
+UENUM(BlueprintType)
+enum class EEffectRemovePolicy : uint8
+{
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
 
 UCLASS()
 class CHAOSDUNGEON_API AChaosDungeonEffectActor : public AActor
@@ -18,16 +37,38 @@ protected:
 	virtual void BeginPlay() override;
 	
 	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
 protected:
-	// EditAnywhere : 用于标记 UCLASS USTRUCT UOBJECT 的成员变量，使其可以在编辑器的详情面板 DetailsPanel 中进行调整
 	// 即时游戏效果类
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	// 即使效果应用策略
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 
 	// 持续时间游戏效果类
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
-	
+	// 持续时间效果应用策略
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	// 无限游戏效果类
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+	// 无限应用策略
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectApplicationPolicy InfinitesEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+	// 无限效果移除策略
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectRemovePolicy InfinitesEffectRemovePolicy = EEffectRemovePolicy::DoNotRemove;
+	// 存储激活的游戏效果句柄及对应的ASC
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveGameplayEffectHandles;
 };
